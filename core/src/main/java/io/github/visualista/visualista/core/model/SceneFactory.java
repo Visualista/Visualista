1afce48859943933292b19b465d7d61453f2d654
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 14:01:04 +0100

    Added GridFactory as field

--- core/src/main/java/io/github/visualista/visualista/core/model/SceneFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/SceneFactory.java
@@ -2,10 +2,14 @@ package io.github.visualista.visualista.core.model;
 
 public class SceneFactory {
     private IdGenerator id = new IdGenerator();
+    private GridFactory gridFactory;
+    
+    public SceneFactory(GridFactory gridFactory){
+        this.gridFactory = gridFactory;
+    }
 
     public Scene createScene() {
 
-        return new Scene(id.generateId());
+        return new Scene(id.generateId(),gridFactory.createGrid());
     }
-
 }
