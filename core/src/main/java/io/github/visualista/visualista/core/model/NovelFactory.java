e88debcb53ea5ec5c1043028056a7b59c38cd6ee
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 13:58:30 +0100

    Added SceneFactory as field

--- core/src/main/java/io/github/visualista/visualista/core/model/NovelFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/NovelFactory.java
@@ -3,12 +3,18 @@ package io.github.visualista.visualista.core.model;
 import java.util.*;
 
 public class NovelFactory {
-    
+
     private IdGenerator id = new IdGenerator();
+
+    private SceneFactory sceneFactory;
+
+    public NovelFactory(SceneFactory sceneFactory) {
+        this.sceneFactory = sceneFactory;
+    }
 
     public Novel createNovel() {
         Map<Integer, Scene> sceneMap = new HashMap<Integer, Scene>();
-        Scene scene = new Scene(id.generateId());
+        Scene scene = sceneFactory.createScene();
         sceneMap.put(scene.getId(), scene);
         return new Novel(sceneMap);
     }
