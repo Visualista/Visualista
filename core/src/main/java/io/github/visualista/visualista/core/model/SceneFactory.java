14ea508b522f959561199d2a939069c97b7df7fa
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 13:31:27 +0100

    Generates a scene with unique id

--- core/src/main/java/io/github/visualista/visualista/core/model/SceneFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/SceneFactory.java
@@ -1,5 +1,11 @@
 package io.github.visualista.visualista.core.model;
 
 public class SceneFactory {
+    private IdGenerator id = new IdGenerator();
+
+    public Scene createScene() {
+
+        return new Scene(id.generateId());
+    }
 
 }
