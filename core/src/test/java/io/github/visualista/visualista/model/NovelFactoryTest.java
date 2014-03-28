8a55dfe76c3b36828aa464f8f260340c6fbd1203
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 12:49:32 +0100

    Test that scene id is the same as the id we requested

--- core/src/test/java/io/github/visualista/visualista/model/NovelFactoryTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/NovelFactoryTest.java
@@ -3,6 +3,7 @@ package io.github.visualista.visualista.model;
 import static org.junit.Assert.*;
 import io.github.visualista.visualista.core.model.Novel;
 import io.github.visualista.visualista.core.model.NovelFactory;
+import io.github.visualista.visualista.core.model.Scene;
 
 import org.junit.Test;
 
@@ -14,6 +15,8 @@ public class NovelFactoryTest {
 		assertNotNull(novel);
 		assertEquals(1,novel.getSceneCount());
 		assertNotNull(novel.getSceneById(1));
+		Scene scene = novel.getSceneById(1);
+		assertEquals(1,scene.getId());
 	}
 
 }
