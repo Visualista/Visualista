ab02bd323db233a85df5a4eb7efcd142271cc539
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 12:51:05 +0100

    Store id internally

--- core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
@@ -1,5 +1,13 @@
 package io.github.visualista.visualista.core.model;
 
 public class Scene {
+    private int id;
 
+    public Scene(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
