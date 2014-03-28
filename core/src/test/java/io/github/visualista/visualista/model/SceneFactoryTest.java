26cb07543a31b483aa1c478c53a1a041e6cbc582
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 14:02:03 +0100

    Added setup method

--- core/src/test/java/io/github/visualista/visualista/model/SceneFactoryTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/SceneFactoryTest.java
@@ -1,16 +1,26 @@
 package io.github.visualista.visualista.model;
 
 import static org.junit.Assert.*;
+import io.github.visualista.visualista.core.model.GridFactory;
+import io.github.visualista.visualista.core.model.NovelFactory;
 import io.github.visualista.visualista.core.model.Scene;
 import io.github.visualista.visualista.core.model.SceneFactory;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SceneFactoryTest {
+    
+    SceneFactory factory;
+    
+    @Before
+    public void setUp() throws Exception {
+        GridFactory gridFactory = new GridFactory();
+        factory = new SceneFactory(gridFactory);
+    }
 
     @Test
     public void test() {
-        SceneFactory factory = new SceneFactory();
         Scene scene = factory.createScene();
         assertNotNull(scene);
     }
