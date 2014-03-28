7f39106ff2c8ef7913c1cc3520ab588e65296a51
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 11:22:06 +0100

    Novel handles a scene list

--- core/src/main/java/io/github/visualista/visualista/core/model/Novel.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Novel.java
@@ -1,5 +1,17 @@
 package io.github.visualista.visualista.core.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Novel {
+    private List<Scene> sceneList;
+
+    public Novel(List<Scene> sceneList) {
+        this.sceneList = sceneList;
+    }
+
+    public int getSceneCount() {
+        return sceneList.size();
+    }
 
 }
