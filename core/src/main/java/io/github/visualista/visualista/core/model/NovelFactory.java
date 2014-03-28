c2508e3d6a38287c4353487518e5a13440169dfb
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 11:22:55 +0100

    Creates a new Novel

--- core/src/main/java/io/github/visualista/visualista/core/model/NovelFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/NovelFactory.java
@@ -1,10 +1,16 @@
 package io.github.visualista.visualista.core.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum NovelFactory {
-	;
-	
-	public static Novel createNovel(){
-		return new Novel();
-	}
+    ;
+
+    public static Novel createNovel() {
+        List<Scene> sceneList = new ArrayList<Scene>();
+        Scene scene = new Scene();
+        sceneList.add(scene);
+        return new Novel(sceneList);
+    }
 
 }
