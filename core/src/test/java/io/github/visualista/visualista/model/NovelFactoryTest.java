8629827697bf671e8227521a86f6ccae821420e3
Jacob Turesson <jacob.thureson@gmail.com>
2014-03-28 14:01:56 +0100

    Added setup method

--- core/src/test/java/io/github/visualista/visualista/model/NovelFactoryTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/NovelFactoryTest.java
@@ -1,17 +1,28 @@
 package io.github.visualista.visualista.model;
 
 import static org.junit.Assert.*;
+import io.github.visualista.visualista.core.model.GridFactory;
 import io.github.visualista.visualista.core.model.Novel;
 import io.github.visualista.visualista.core.model.NovelFactory;
 import io.github.visualista.visualista.core.model.Scene;
+import io.github.visualista.visualista.core.model.SceneFactory;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class NovelFactoryTest {
+    
+    private NovelFactory factory;
+    
+    @Before
+    public void setUp() throws Exception {
+        GridFactory gridFactory = new GridFactory();
+        SceneFactory sceneFactory = new SceneFactory(gridFactory);
+        factory = new NovelFactory(sceneFactory);
+    }
 
 	@Test
 	public void testCreateNovel() {
-	    NovelFactory factory = new NovelFactory();
 	    Novel novel = factory.createNovel();
 		assertNotNull(novel);
 		assertEquals(1,novel.getSceneCount());
