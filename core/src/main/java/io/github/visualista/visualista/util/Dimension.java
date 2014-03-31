56cab37662d03eeec957bacabd31abc3a56bf08a
Pierre Krafft <krafftpierre@gmail.com>
2014-03-31 15:07:26 +0200

    Changed equals method to pass tester

---
+++ core/src/main/java/io/github/visualista/visualista/util/Dimension.java
@@ -0,0 +1,58 @@
+package io.github.visualista.visualista.util;
+
+public class Dimension {
+	private final int width;
+	private final int height;
+	
+
+	public Dimension(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Dimension [width=" + width + ", height=" + height + "]";
+	}
+
+
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + height;
+		result = prime * result + width;
+		return result;
+	}
+
+
+	@Override
+	public final boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Dimension))
+			return false;
+		Dimension other = (Dimension) obj;
+		if (height != other.height)
+			return false;
+		if (width != other.width)
+			return false;
+		return true;
+	}
+
+
+
+}
