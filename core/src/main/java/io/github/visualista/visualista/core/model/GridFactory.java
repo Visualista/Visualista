ee779982acf9bbc36d2c09382dd8edd978199372
Pierre Krafft <krafftpierre@gmail.com>
2014-03-31 10:43:58 +0200

    Create grid with size 5x5

--- core/src/main/java/io/github/visualista/visualista/core/model/GridFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/GridFactory.java
@@ -3,7 +3,7 @@ package io.github.visualista.visualista.core.model;
 public class GridFactory {
 
     public Grid createGrid() {
-        return new Grid();
+        return new Grid(5,5);
     }
 
 }
