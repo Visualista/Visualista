1edf034cf240d494d3c42ad971d6230ada69b675
Pierre Krafft <krafftpierre@gmail.com>
2014-03-31 10:42:50 +0200

    Pass test for dimension size and constructors

--- core/src/main/java/io/github/visualista/visualista/core/model/Grid.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Grid.java
@@ -1,5 +1,18 @@
 package io.github.visualista.visualista.core.model;
 
 public class Grid {
+	private final Dimension gridSize;
+	
+	public Grid(Dimension gridSize){
+		this.gridSize = gridSize;
+	}
+	
+	public Grid(int width, int height){
+		this.gridSize = new Dimension(width,height);
+	}
+	
+	public Dimension getSize() {
+		return gridSize;
+	}
 
 }
