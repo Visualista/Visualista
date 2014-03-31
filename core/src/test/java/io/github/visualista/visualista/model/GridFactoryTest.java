7edd2fb4fc6c3e63b2cff1d437710091d6a5af55
Pierre Krafft <krafftpierre@gmail.com>
2014-03-31 10:02:20 +0200

    Created GridFactoryTest

---
+++ core/src/test/java/io/github/visualista/visualista/model/GridFactoryTest.java
@@ -0,0 +1,14 @@
+package io.github.visualista.visualista.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridFactoryTest {
+
+	@Test
+	public void testCreateGrid() {
+		fail("Not yet implemented");
+	}
+
+}
