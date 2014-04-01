f38c32ca59c2cdfb86a8147349598adce1d49738
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 13:53:45 +0200

    Set up ActorFactory

--- core/src/main/java/io/github/visualista/visualista/core/model/ActorFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/ActorFactory.java
@@ -2,4 +2,14 @@ package io.github.visualista.visualista.core.model;
 
 public class ActorFactory {
 
+	private final IdGenerator id;
+	
+	public ActorFactory() {
+		id = new IdGenerator();
+	}
+	
+	public Actor createActor(){
+		int actorId = id.generateId();
+		return new Actor();
+	}
 }
