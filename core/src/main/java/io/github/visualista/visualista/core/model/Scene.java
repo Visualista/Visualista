896afaad8d5695ff7a70d35007ab823042a9bd28
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 17:44:57 +0200

    Added ID compability

--- core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
@@ -1,12 +1,14 @@
 package io.github.visualista.visualista.core.model;
 
+import io.github.visualista.visualista.util.Identifiable;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Scene {
+public class Scene implements Identifiable{
     private final int id;
     private final Grid grid;
-    private final List<Integer> actorsInScene;
+    private final List<Integer> actorsInScene; //Note that actors are referenced by IDs
 
     public Scene(int id, Grid grid) {
         this.id = id;
