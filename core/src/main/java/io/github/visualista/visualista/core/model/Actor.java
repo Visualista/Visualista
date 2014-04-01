f3f6451af3126146ebc3688ec4c87993ec73e2ea
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 13:58:42 +0200

    Added constructor to Actor

--- core/src/main/java/io/github/visualista/visualista/core/model/Actor.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Actor.java
@@ -1,10 +1,15 @@
 package io.github.visualista.visualista.core.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Actor {
 	public static final Actor EMPTY_ACTOR = new Actor();
-	Image image;
-	List<Action> actions;
+	private Image image;
+	private final List<Action> actions;
 	
+	public Actor(){
+		actions = new ArrayList<Action>();
+		image = new Image();
+	}
 }
