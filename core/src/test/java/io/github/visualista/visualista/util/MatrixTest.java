4b8b8234224fcce1eee5e365552e4c6053ad413e
Pierre Krafft <krafftpierre@gmail.com>
2014-04-01 13:16:55 +0200

    Formatting

--- core/src/test/java/io/github/visualista/visualista/util/MatrixTest.java
+++ core/src/test/java/io/github/visualista/visualista/util/MatrixTest.java
@@ -8,26 +8,24 @@ import org.junit.Test;
 
 public class MatrixTest {
 
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void matrixHeightShouldBeGreaterThan1() {
-		new Matrix<Object>(new Dimension(-1,1));
+		new Matrix<Object>(new Dimension(-1, 1));
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void matrixWidthShouldBeGreaterThan1() {
-		new Matrix<Object>(new Dimension(1,-1));
+		new Matrix<Object>(new Dimension(1, -1));
 	}
-	
 
 	@Test
 	public void testGetSize() {
 		Random rand = new Random();
-		int width = rand.nextInt(100)+1;
-		int height = rand.nextInt(1000)+1;
-		Dimension dim = new Dimension(width,height);
+		int width = rand.nextInt(100) + 1;
+		int height = rand.nextInt(1000) + 1;
+		Dimension dim = new Dimension(width, height);
 		Matrix<Object> matrix = new Matrix<Object>(dim);
-		assertEquals(dim,matrix.getSize());
-		
+		assertEquals(dim, matrix.getSize());
 	}
 
 }
