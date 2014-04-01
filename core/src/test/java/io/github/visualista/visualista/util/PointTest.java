1601bd88737d9daa9615e302e3f8b8d50775a6d6
Pierre Krafft <krafftpierre@gmail.com>
2014-04-01 14:02:11 +0200

    Copied test for Dimension. The classes are just aliases

---
+++ core/src/test/java/io/github/visualista/visualista/util/PointTest.java
@@ -0,0 +1,46 @@
+package io.github.visualista.visualista.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import io.github.visualista.visualista.util.Point;
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PointTest {
+	private Point point;
+	private int width;
+	private int y;
+
+	@Before
+	public void setUp() throws Exception {
+		Random rand = new Random();
+		width = rand.nextInt();
+		y = rand.nextInt();
+		point = new Point(width, y);
+	}
+
+	@Test
+	public void testGetX() {
+		assertEquals(width, point.getX());
+	}
+
+	@Test
+	public void testGetY() {
+		assertEquals(y, point.getY());
+	}
+
+	@Test
+	public void equalsContract() {
+		EqualsVerifier.forClass(Point.class).verify();
+	}
+
+	@Test
+	public void testToString() {
+		assertTrue(point.toString().contains("" + width));
+		assertTrue(point.toString().contains("" + y));
+	}
+}
