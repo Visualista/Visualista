edc33b525a8230e2ce2e3fa39d2b6a134d95befc
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 14:09:21 +0200

    Added support for ID in Actor

--- core/src/main/java/io/github/visualista/visualista/core/model/Actor.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Actor.java
@@ -7,9 +7,15 @@ public class Actor {
 	public static final Actor EMPTY_ACTOR = new Actor();
 	private Image image;
 	private final List<Action> actions;
+	private final int id;
 	
-	public Actor(){
+	public Actor(int id){
+		this.id = id;
 		actions = new ArrayList<Action>();
 		image = new Image();
+	}
+	
+	public int getId(){
+		return id;
 	}
 }
