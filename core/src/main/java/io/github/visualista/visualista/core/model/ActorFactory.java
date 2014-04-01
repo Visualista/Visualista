ff26529b0206afb371bac78cc973759f27407495
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 14:06:50 +0200

    Added support for ID in ActorFactory

--- core/src/main/java/io/github/visualista/visualista/core/model/ActorFactory.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/ActorFactory.java
@@ -2,14 +2,14 @@ package io.github.visualista.visualista.core.model;
 
 public class ActorFactory {
 
-	private final IdGenerator id;
+	private final IdGenerator idGen;
 	
 	public ActorFactory() {
-		id = new IdGenerator();
+		idGen = new IdGenerator();
 	}
 	
 	public Actor createActor(){
-		int actorId = id.generateId();
-		return new Actor();
+		int id = idGen.generateId();
+		return new Actor(id);
 	}
 }
