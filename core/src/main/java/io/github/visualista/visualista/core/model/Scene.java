d3211dd1a60d9faae612c6b0a46df1f77979629c
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 17:00:02 +0200

    Cleared up errors

--- core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
@@ -1,12 +1,17 @@
 package io.github.visualista.visualista.core.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scene {
     private final int id;
     private final Grid grid;
+    private final List<Integer> actorsInScene;
 
     public Scene(int id, Grid grid) {
         this.id = id;
         this.grid = grid;
+        actorsInScene = new ArrayList<Integer>();
     }
 
     public int getId() {
