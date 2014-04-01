ce7ca6a42f7455cc8a95608b1b4319883d775f26
Markus Bergland <markus.bergland@gmail.com>
2014-04-01 18:24:56 +0200

    Made class rely on XStreamManager

--- core/src/main/java/io/github/visualista/visualista/util/ReferenceManager.java
+++ core/src/main/java/io/github/visualista/visualista/util/ReferenceManager.java
@@ -18,25 +18,25 @@ import com.thoughtworks.xstream.XStream;
 public class ReferenceManager<E extends Identifiable> {
 
 	private final ObjectFactory<E> objectFactory;	
-	private final XStream xstream;
+	private XStream xstream;
 	private Map<Integer, E> objectMap;
 	
-	public ReferenceManager(File folder){
-		xstream = new XStream();
+	public ReferenceManager(File folder, XStream xstream){
+		this.xstream = xstream;
 		objectFactory = new ObjectFactory<E>(xstream);
 		objectMap = new HashMap<Integer, E>();
 		
-		if (!folder.isDirectory()){
-			throw new IllegalArgumentException("Not a directory");
-		} else if (!folder.exists()){
+		if (!folder.exists()){
 			folder.mkdirs();
 			System.out.println("Creating " + folder.toString());
+		}else if (!folder.isDirectory()){
+			throw new IllegalArgumentException("Not a directory!");
 		} else {
 			createObjects(folder);
 		}
 		
 	}
-	
+
 	private void createObjects(File folder){
 		for (final File file : folder.listFiles()){
 			if(!file.isDirectory()){
