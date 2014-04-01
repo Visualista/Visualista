eb8cd58851c27bd92f80dbbc3e5b5c13bc111cce
Pierre Krafft <krafftpierre@gmail.com>
2014-04-01 11:26:35 +0200

    Is same as Dimension, but has different name for width and height. Used to get a nicer domain language. See if there is a better way to do this?

---
+++ core/src/main/java/io/github/visualista/visualista/util/Point.java
@@ -0,0 +1,57 @@
+package io.github.visualista.visualista.util;
+
+public class Point {
+	private final int x;
+	private final int y;
+	
+
+	public Point(int width, int height) {
+		this.x = width;
+		this.y = height;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + y;
+		result = prime * result + x;
+		return result;
+	}
+
+
+	@Override
+	public final boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Point))
+			return false;
+		Point other = (Point) obj;
+		if (y != other.y)
+			return false;
+		if (x != other.x)
+			return false;
+		return true;
+	}
+
+
+
+}
