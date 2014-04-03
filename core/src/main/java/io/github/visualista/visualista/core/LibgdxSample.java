44a42fd2bd32d7659939c6c5fd482654e99a9b97
Jacob Turesson <jacob.thureson@gmail.com>
2014-04-03 16:32:33 +0200

    Adding left and right vertical alignment

--- core/src/main/java/io/github/visualista/visualista/core/LibgdxSample.java
+++ core/src/main/java/io/github/visualista/visualista/core/LibgdxSample.java
@@ -2,44 +2,54 @@ package io.github.visualista.visualista.core;
 
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 
 public class LibgdxSample implements ApplicationListener {
-	Texture texture;
-	SpriteBatch batch;
-	float elapsed;
+    private Stage stage;
+    private VerticalGroup leftGroup;
+    private VerticalGroup rightGroup;
 
-	@Override
-	public void create () {
-		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
-		batch = new SpriteBatch();
-	}
+    @Override
+    public void create() {
+        stage = new Stage(1900, 1000, true);
+        stage.clear();
 
-	@Override
-	public void resize (int width, int height) {
-	}
+        leftGroup = new VerticalGroup();
+        rightGroup = new VerticalGroup();
+        stage.addActor(leftGroup);
+        stage.addActor(rightGroup);
+    }
 
-	@Override
-	public void render () {
-		elapsed += Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
-		batch.end();
-	}
+    @Override
+    public void resize(int width, int height) {
+    }
 
-	@Override
-	public void pause () {
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-	@Override
-	public void resume () {
-	}
+    }
 
-	@Override
-	public void dispose () {
-	}
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
