56c17c09d9cd47b3d853f94ce800371aa453edc3
Pierre Krafft <krafftpierre@gmail.com>
2014-04-03 10:24:36 +0200

    Comment out the test that was used to print debug info. Addde TODO

--- core/src/test/java/io/github/visualista/visualista/io/CollectionLoaderTest.java
+++ core/src/test/java/io/github/visualista/visualista/io/CollectionLoaderTest.java
@@ -16,7 +16,8 @@ public class CollectionLoaderTest {
 		XStreamManager manager = new XStreamManager();
 		CollectionLoader<Actor> loader = new CollectionLoader<Actor>(manager.getMainXStream(), new File("C:\\VISUALISTA\\"));
 		Collection<Actor> loadedObjects = loader.getObjects();
-		assertEquals(loadedObjects.toString(),"");
+		//assertEquals(loadedObjects.toString(),"");
+		//TODO make testable. Will probably need mock objects and so on
 	}
 
 }
