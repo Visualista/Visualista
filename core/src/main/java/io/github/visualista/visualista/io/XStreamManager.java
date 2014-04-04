517a04a2bf157253eb6c19d7886101636de91549
Pierre Krafft <krafftpierre@gmail.com>
2014-04-04 11:55:03 +0200

    Prettier output added for classes that needed it

--- core/src/main/java/io/github/visualista/visualista/io/XStreamManager.java
+++ core/src/main/java/io/github/visualista/visualista/io/XStreamManager.java
@@ -3,9 +3,12 @@ package io.github.visualista.visualista.io;
 
 import io.github.visualista.visualista.core.model.Action;
 import io.github.visualista.visualista.core.model.Actor;
+import io.github.visualista.visualista.core.model.Grid;
 import io.github.visualista.visualista.core.model.Novel;
 import io.github.visualista.visualista.core.model.Scene;
 import io.github.visualista.visualista.core.model.Tile;
+import io.github.visualista.visualista.util.Matrix;
+import io.github.visualista.visualista.util.Row;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -24,6 +27,9 @@ public class XStreamManager {
 		mainXStream.alias("Scene", Scene.class);
 		mainXStream.alias("Novel", Novel.class);
 		mainXStream.alias("Tile", Tile.class);
+		mainXStream.alias("Row", Row.class);
+		mainXStream.addImplicitArray(Row.class, "row");
+		mainXStream.addImplicitArray(Matrix.class, "matrix");
 		
 	}
 	
