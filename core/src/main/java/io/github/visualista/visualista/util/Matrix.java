905fb26ac4c9e1552d42d21a17f0d90a93aa9e89
Pierre Krafft <krafftpierre@gmail.com>
2014-04-04 11:54:24 +0200

    Updated to use Row

--- core/src/main/java/io/github/visualista/visualista/util/Matrix.java
+++ core/src/main/java/io/github/visualista/visualista/util/Matrix.java
@@ -1,7 +1,7 @@
 package io.github.visualista.visualista.util;
 
 public class Matrix<E> implements IMatrixGet<E>,IMatrixSet<E> {
-	private final E[][] matrix;
+	private final Row<E>[] matrix;
 
 	@SuppressWarnings("unchecked")
 	public Matrix(Dimension size) {
@@ -11,18 +11,21 @@ public class Matrix<E> implements IMatrixGet<E>,IMatrixSet<E> {
 		if(size.getWidth()<1){
 			throw new IllegalArgumentException("Matrix width <1 is not allowed. Sent in was: "+size.getWidth());
 		}
-		matrix = (E[][])new Object[size.getHeight()][size.getWidth()];
+		matrix = (Row<E>[])new Row[size.getHeight()];
+		for(int i = 0;i<matrix.length;++i){
+			matrix[i] = new Row(size.getWidth());
+		}
 	}
 	
 	public Dimension getSize(){
-		return new Dimension(matrix[0].length,matrix.length);
+		return new Dimension(matrix[0].getLength(),matrix.length);
 	}
 
 	@Override
 	public void fillWith(IObjectCreator<E> creator) {
 		for(int i=0;i<matrix.length;++i){
-			for(int j=0;j<matrix[i].length;++j){
-				matrix[i][j] = creator.createObject();
+			for(int j=0;j<matrix[i].getLength();++j){
+				matrix[i].setAt(j, creator.createObject());
 			}
 		}
 		
@@ -30,12 +33,12 @@ public class Matrix<E> implements IMatrixGet<E>,IMatrixSet<E> {
 
 	@Override
 	public E getAt(Point from) {
-		return matrix[from.getY()][from.getX()];
+		return matrix[from.getY()].getAt(from.getX());
 	}
 
 	@Override
 	public void setAt(Point where, E object) {
-		matrix[where.getY()][where.getX()] = object;
+		matrix[where.getY()].setAt(where.getX(), object);
 	}
 
 }
