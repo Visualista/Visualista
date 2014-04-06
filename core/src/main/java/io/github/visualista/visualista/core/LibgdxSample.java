d52cf42e7a81455b016da07198889281273b4b63
Jacob Turesson <jacob.thureson@gmail.com>
2014-04-06 21:00:19 +0200

    Adding buttons to the stage

--- core/src/main/java/io/github/visualista/visualista/core/LibgdxSample.java
+++ core/src/main/java/io/github/visualista/visualista/core/LibgdxSample.java
@@ -1,5 +1,8 @@
 package io.github.visualista.visualista.core;
 
+import java.io.File;
+import java.util.logging.FileHandler;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -17,31 +20,52 @@ import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 
 public class LibgdxSample implements ApplicationListener {
     private Stage stage;
-    private VerticalGroup leftGroup;
-    private VerticalGroup rightGroup;
+
 
     @Override
     public void create() {
-        stage = new Stage(1900, 1000, true);
+        stage = new Stage(1800, 900, true);
         stage.clear();
-        
-        leftGroup = new VerticalGroup();
-        rightGroup = new VerticalGroup();
-        stage.addActor(leftGroup);
-        stage.addActor(rightGroup);
-      
-        Drawable draw = new TextureRegionDrawable(new TextureRegion(new Texture("libgdx-logo.png")));
-        ImageButton button = new ImageButton(draw);
-        button.setX(500);
-        button.setY(500);
-        button.setWidth(250);
-        button.setHeight(200);
-        stage.addActor(button);
-        button.setBackground(draw);
+
+        //Skin uiSkin = new Skin(Gdx.files.internal("uiskin.json.txt"));
+        //Label label = new Label("Hej", uiSkin);
+        //label.setX(500);
+        //label.setY(500);
+        //label.setWidth(100);
+        //label.setHeight(100);
+        //stage.addActor(label);
+
+        Drawable cursor = new TextureRegionDrawable(new TextureRegion(
+                new Texture(Gdx.files.internal("icons/cursor.png"))));
+        ImageButton leftButton = new ImageButton(cursor);
+        leftButton.setX(69);
+        leftButton.setY(100);
+        leftButton.setWidth(100);
+        leftButton.setHeight(100);
+        stage.addActor(leftButton);
+
+        Drawable hand = new TextureRegionDrawable(new TextureRegion(
+                new Texture(Gdx.files.internal("icons/hand.png"))));
+        ImageButton centerButton = new ImageButton(hand);
+        leftButton.setX(171);
+        leftButton.setY(100);
+        leftButton.setWidth(100);
+        leftButton.setHeight(100);
+        stage.addActor(centerButton);
+
+        Drawable arrow = new TextureRegionDrawable(new TextureRegion(
+                new Texture(Gdx.files.internal("icons/arrow.png"))));
+        ImageButton rightButton = new ImageButton(arrow);
+        leftButton.setX(273);
+        leftButton.setY(100);
+        leftButton.setWidth(100);
+        leftButton.setHeight(100);
+        stage.addActor(rightButton);
     }
 
     @Override
