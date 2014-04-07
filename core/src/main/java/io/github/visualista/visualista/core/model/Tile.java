9a064d63f599d700085888e378e61c35d476518c
Pierre Krafft <krafftpierre@gmail.com>
2014-04-07 10:45:51 +0200

    Use references

--- core/src/main/java/io/github/visualista/visualista/core/model/Tile.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Tile.java
@@ -2,28 +2,28 @@ package io.github.visualista.visualista.core.model;
 
 
 public class Tile {
-	//private Actor tileActor;
-	int actorId = 0;
+	private Actor tileActor;
+	//int actorId = 0;
 	
 	
 	public Tile(Actor tileActor){
-		//this.tileActor = tileActor;
-		if(tileActor!=null){
-			actorId = tileActor.getId();
-		}
-	}
-	/*
-	public Actor getActor(){
-		return tileActor;
-	}*/
-	public int getActorId(){
-		return actorId;
+		this.tileActor = tileActor;
+		//if(tileActor!=null){
+		//	actorId = tileActor.getId();
+		//}
 	}
 	
+	public Actor getActor(){
+		return tileActor;
+	}
+	//public int getActorId(){
+	//	return actorId;
+	//}
+	
 	public void setActor(Actor tileActor){
-		//this.tileActor = tileActor;
-		if(tileActor!=null){
-			actorId = tileActor.getId();
-		}
+		this.tileActor = tileActor;
+		//if(tileActor!=null){
+		//	actorId = tileActor.getId();
+		//}
 	}
 }
