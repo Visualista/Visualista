7b602bd95dbf12e5844aa419f586d845a391e4ed
Markus Bergland <markus.bergland@gmail.com>
2014-04-07 16:09:35 +0200

    Added stuffs

---
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
@@ -0,0 +1,15 @@
+package io.github.visualista.visualista.controller;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class EditorController implements PropertyChangeListener{
+
+	@Override
+	public void propertyChange(PropertyChangeEvent pce) {
+		
+		
+	}
+
+	
+}
