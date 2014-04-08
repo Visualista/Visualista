9b4ecf7045ebc8d3150e878ed8be7064dafa86ae
Erik Risfelt <fredokin@gmail.com>
2014-04-08 15:14:14 +0200

    Added more tests to SceneTest

--- core/src/test/java/io/github/visualista/visualista/model/SceneTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/SceneTest.java
@@ -1,45 +1,51 @@
 package io.github.visualista.visualista.model;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-import io.github.visualista.visualista.core.model.Actor;
-import io.github.visualista.visualista.core.model.GridFactory;
-import io.github.visualista.visualista.core.model.Scene;
+import io.github.visualista.visualista.core.model.*;
 
-
-
-
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-
-import org.hamcrest.core.StringContains;
-import org.hamcrest.text.StringContainsInOrder;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SceneTest {
     
     private GridFactory gridFactory;
+    private SceneFactory sceneFactory;
 
     @Before
     public void setUp() throws Exception {
         gridFactory = new GridFactory();
+        sceneFactory = new SceneFactory(gridFactory);
     }
 
     @Test
     public void test() {
-        Scene scene = new Scene(gridFactory.createGrid(),new ArrayList<Actor>());
+        Scene scene = sceneFactory.createScene();
         assertNotNull(scene.getGrid());
     }
     
     @Test
 	public void testGetAndSetName() {
-		Scene scene = new Scene(gridFactory.createGrid(),new ArrayList<Actor>());
+		Scene scene = sceneFactory.createScene();
 		String testString = "Something, something, dark side";
 		scene.setName(testString);
-		assertEquals(testString, scene.getName());
+		assertThat(testString, equalTo(scene.getName()));
 	}
+    
+    @Test
+    public void testSetAndGetStoryText(){
+    	Scene scene = sceneFactory.createScene();
+    	String testString = "Something, something, evil";
+    	scene.setStoryText(testString);
+    	assertThat(scene.getStoryText(),equalTo(testString));
+    }
+    
+    @Test
+    public void testAddActorAndGetActorsInScene(){
+    	Scene scene = sceneFactory.createScene();
+    	Actor actor = new ActorFactory().createActor();
+    	scene.addActor(actor);
+    	assertThat(actor, equalTo(scene.getActorsInScene().get(0)));
+    }
 
 }
