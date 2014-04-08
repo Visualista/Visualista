deffd04767d7f939bc8981ecc0ac8a9700cc0e16
Erik Risfelt <fredokin@gmail.com>
2014-04-08 15:16:39 +0200

    Code refractoring in SceneTest

--- core/src/test/java/io/github/visualista/visualista/model/SceneTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/SceneTest.java
@@ -11,38 +11,36 @@ public class SceneTest {
     
     private GridFactory gridFactory;
     private SceneFactory sceneFactory;
+    private Scene scene;
+    private String testString;
 
     @Before
     public void setUp() throws Exception {
         gridFactory = new GridFactory();
         sceneFactory = new SceneFactory(gridFactory);
+        scene = sceneFactory.createScene();
+        testString = "Something creative";
     }
 
     @Test
     public void test() {
-        Scene scene = sceneFactory.createScene();
         assertNotNull(scene.getGrid());
     }
     
     @Test
 	public void testGetAndSetName() {
-		Scene scene = sceneFactory.createScene();
-		String testString = "Something, something, dark side";
 		scene.setName(testString);
 		assertThat(testString, equalTo(scene.getName()));
 	}
     
     @Test
     public void testSetAndGetStoryText(){
-    	Scene scene = sceneFactory.createScene();
-    	String testString = "Something, something, evil";
     	scene.setStoryText(testString);
     	assertThat(scene.getStoryText(),equalTo(testString));
     }
     
     @Test
     public void testAddActorAndGetActorsInScene(){
-    	Scene scene = sceneFactory.createScene();
     	Actor actor = new ActorFactory().createActor();
     	scene.addActor(actor);
     	assertThat(actor, equalTo(scene.getActorsInScene().get(0)));
