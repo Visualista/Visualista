8df9837141fe47b6da2e918bc9aa397e9be48213
Erik Risfelt <fredokin@gmail.com>
2014-04-08 14:40:57 +0200

    Added storyText String to Scene

--- core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Scene.java
@@ -7,8 +7,17 @@ public class Scene{
     
     private final List<Actor> actorsInScene;
     private String name;
+    private String storyText;
 
-    public String getName() {
+    public String getStoryText() {
+		return storyText;
+	}
+
+	public void setStoryText(String storyText) {
+		this.storyText = storyText;
+	}
+
+	public String getName() {
 		return name;
 	}
 
