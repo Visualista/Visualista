d34230d80c23ebdeb937a900815fc897504cf027
Markus Bergland <markus.bergland@gmail.com>
2014-04-08 14:10:16 +0200

    Created

---
+++ core/src/main/java/io/github/visualista/visualista/controller/Director.java
@@ -0,0 +1,19 @@
+package io.github.visualista.visualista.controller;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Director implements ActionListener{
+	
+	public Director(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// TODO Stuff
+		
+	}
+
+}
