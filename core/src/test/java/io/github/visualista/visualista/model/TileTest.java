59e24615fed78ae07f51e7718a13af5c1fcdc8bc
Erik Risfelt <fredokin@gmail.com>
2014-04-08 15:35:08 +0200

    Added more tests to TileTest

--- core/src/test/java/io/github/visualista/visualista/model/TileTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/TileTest.java
@@ -3,17 +3,36 @@ package io.github.visualista.visualista.model;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import io.github.visualista.visualista.core.model.Actor;
+import io.github.visualista.visualista.core.model.ActorFactory;
 import io.github.visualista.visualista.core.model.Tile;
+import io.github.visualista.visualista.core.model.TileFactory;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TileTest {
+	
+	private TileFactory tileFactory;
+	private Tile tile;
 
-    @Test
+    @Before
+    public void setUp() throws Exception{
+    	tileFactory = new TileFactory();
+    	tile = tileFactory.createTile();
+    }
+    
+	
+	@Test
     public void test() {
-        Tile tile = new Tile(new Actor());
+        assertThat(tile,notNullValue());
         assertThat(tile.getActor(),notNullValue());
     }
     
-
+    @Test
+    public void testSetAndGetActor() {
+    	Actor actor = new ActorFactory().createActor();
+    	tile.setActor(actor);
+    	assertThat(actor,equalTo(tile.getActor()));
+    }
+    
 }
