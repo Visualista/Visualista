8dd31f1eb54eacc5f62e5b75218dac93dc70a753
Markus Bergland <markus.bergland@gmail.com>
2014-04-08 15:34:07 +0200

    Fixed skins

--- desktop/src/main/java/io/github/visualista/visualista/java/VisualistaDesktop.java
+++ desktop/src/main/java/io/github/visualista/visualista/java/VisualistaDesktop.java
@@ -3,14 +3,14 @@ package io.github.visualista.visualista.java;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import io.github.visualista.visualista.core.LibgdxSample;
+import io.github.visualista.visualista.core.TestMenu;
 
 public class VisualistaDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
-		config.width = 1800;
-		config.height = 900;
-		new LwjglApplication(new LibgdxSample(), config);
+		config.width = 600;
+		config.height = 600;
+		new LwjglApplication(new TestMenu(), config);
 	}
 }
