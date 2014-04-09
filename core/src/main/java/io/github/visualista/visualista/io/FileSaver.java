db0c30d1a78f2c7ce00f9e354921048a3cf7c44d
Pierre Krafft <krafftpierre@gmail.com>
2014-04-09 16:19:40 +0200

    Saves one object to one file

---
+++ core/src/main/java/io/github/visualista/visualista/io/FileSaver.java
@@ -0,0 +1,29 @@
+package io.github.visualista.visualista.io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.thoughtworks.xstream.XStream;
+
+public class FileSaver<E> {
+
+	private final XStream xstream;
+
+	public FileSaver(XStream xstream) {
+		this.xstream = xstream;
+	}
+	
+	public void saveObjectToFile(File outputFile, E object){
+		try {
+			FileOutputStream fos = new FileOutputStream(outputFile);
+			try {
+				xstream.toXML(object, fos);
+			} finally {
+				fos.close();
+			}
+		} catch (IOException e) {
+		}
+	}
+
+}
