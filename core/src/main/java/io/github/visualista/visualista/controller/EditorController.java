83a1b71bc53903569e31ea4eba0f595258227233
Erik Risfelt <fredokin@gmail.com>
2014-04-18 14:12:44 +0200

    Minor edits to EditorController

--- core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
@@ -1,14 +1,24 @@
 package io.github.visualista.visualista.controller;
 
+import io.github.visualista.visualista.core.Visualista;
+
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 
 public class EditorController implements EventListener{
+	
+	private Visualista visualista;
 
 	@Override
 	public boolean handle(Event event) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	public EditorController (Visualista visualista){
+		this.visualista = visualista;
+	}
+	
+	
 
 }
