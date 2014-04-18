d2fd91fab837747ea56928954b1f9123026369d5
Erik Risfelt <fredokin@gmail.com>
2014-04-18 13:02:45 +0200

    Added IEditNovel interface

--- core/src/main/java/io/github/visualista/visualista/core/model/Novel.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/Novel.java
@@ -4,7 +4,7 @@ import io.github.visualista.visualista.util.Nameable;
 
 import java.util.*;
 
-public class Novel implements Nameable {
+public class Novel implements Nameable, IEditNovel {
 	List<Scene> scenes;
 	private String name;
 
