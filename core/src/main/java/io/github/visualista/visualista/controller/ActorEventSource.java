c9727cf6f8b77c5032bc5484d2f7115b90a7a75d
Markus Bergland <markus.bergland@gmail.com>
2014-04-18 15:04:02 +0200

    Created

---
+++ core/src/main/java/io/github/visualista/visualista/controller/ActorEventSource.java
@@ -0,0 +1,25 @@
+package io.github.visualista.visualista.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ActorEventSource {
+	private final List<ActorListener> actorEventListeners = new ArrayList<ActorListener>();
+	
+	public synchronized void addEventListener(ActorListener actorListener){
+		actorEventListeners.add(actorListener);
+	}
+	
+	public synchronized void removeEventListener(ActorListener actorListener){
+		actorEventListeners.remove(actorListener);
+	}
+	
+	private synchronized void fireActorEvent(){
+		ActorEvent event = new ActorEvent(this);
+		Iterator<ActorListener> eventIterator = actorEventListeners.iterator();
+		while(eventIterator.hasNext()){
+			eventIterator.next().actorCalled(event);
+		}
+	}
+}
