fdfd509b4eb42bb142d4358e3661b494c50ffe30
Pierre Krafft <krafftpierre@gmail.com>
2014-05-05 23:12:15 +0200

    Changed to correct view class. Magic numbers removed.

--- html/src/main/java/io/github/visualista/visualista/html/VisualistaHtml.java
+++ html/src/main/java/io/github/visualista/visualista/html/VisualistaHtml.java
@@ -1,19 +1,22 @@
 package io.github.visualista.visualista.html;
 
-import io.github.visualista.visualista.core.LibgdxSample;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
+import io.github.visualista.view.VisualistaView;
+
 public class VisualistaHtml extends GwtApplication {
+	private static final int APPLICATION_HEIGHT = 320;
+	private static final int APPICATION_WIDTH = 480;
+
 	@Override
 	public ApplicationListener getApplicationListener() {
-		return new LibgdxSample();
+		return new VisualistaView();
 	}
 
 	@Override
 	public GwtApplicationConfiguration getConfig() {
-		return new GwtApplicationConfiguration(480, 320);
+		return new GwtApplicationConfiguration(APPICATION_WIDTH, APPLICATION_HEIGHT);
 	}
 }
