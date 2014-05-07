3902a15f0a93512296bd9ce64b7ba66df35f52cc
Pierre Krafft <krafftpierre@gmail.com>
2014-05-08 00:35:31 +0200

    Added Type to the event

--- core/src/main/java/io/github/visualista/visualista/controller/EditorViewEvent.java
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorViewEvent.java
@@ -3,9 +3,21 @@ package io.github.visualista.visualista.controller;
 import java.util.EventObject;
 
 public class EditorViewEvent extends EventObject {
+	private static final long serialVersionUID = -8033356189055577154L;
 
-	public EditorViewEvent(Object source) {
+	enum Type{
+		NEW_SCENE
+	}
+
+	private final Type eventType;
+
+	public EditorViewEvent(final Object source, final Type eventType) {
 		super(source);
+		this.eventType = eventType;
+	}
+
+	public final Type getEventType() {
+		return eventType;
 	}
 
 }
