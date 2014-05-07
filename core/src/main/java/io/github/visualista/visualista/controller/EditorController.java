285b7c9f161946a696de63c9838345161e514804
Pierre Krafft <krafftpierre@gmail.com>
2014-05-07 20:55:28 +0200

    VisualistaView added to controller

--- core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
@@ -1,5 +1,6 @@
 package io.github.visualista.visualista.controller;
 
+import io.github.visualista.view.VisualistaView;
 import io.github.visualista.visualista.core.Visualista;
 
 import com.badlogic.gdx.scenes.scene2d.Event;
@@ -8,15 +9,18 @@ import com.badlogic.gdx.scenes.scene2d.EventListener;
 public class EditorController implements EventListener {
 
 	private Visualista visualista;
+	private VisualistaView view;
+
+	public EditorController(final Visualista visualista,
+			final VisualistaView view) {
+		this.visualista = visualista;
+		this.view = view;
+	}
 
 	@Override
 	public boolean handle(Event event) {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	public EditorController(Visualista visualista) {
-		this.visualista = visualista;
 	}
 
 }
