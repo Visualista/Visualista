29276ca4fb19379ffd95c3c23b18e81f39317eef
Pierre Krafft <krafftpierre@gmail.com>
2014-05-07 21:39:19 +0200

    Created dummy ViewEvent

---
+++ core/src/main/java/io/github/visualista/visualista/controller/ViewEvent.java
@@ -0,0 +1,11 @@
+package io.github.visualista.visualista.controller;
+
+import java.util.EventObject;
+
+public class ViewEvent extends EventObject {
+
+	public ViewEvent(Object source) {
+		super(source);
+	}
+
+}
