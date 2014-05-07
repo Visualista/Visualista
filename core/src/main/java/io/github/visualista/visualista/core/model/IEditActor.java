baf9a415143f04606d7eae0c1adc9ab09a8f74f4
Pierre Krafft <krafftpierre@gmail.com>
2014-05-07 20:56:26 +0200

    IEditActor separated into IGetActor to make get-only access possible

--- core/src/main/java/io/github/visualista/visualista/core/model/IEditActor.java
+++ core/src/main/java/io/github/visualista/visualista/core/model/IEditActor.java
@@ -1,16 +1,8 @@
 package io.github.visualista.visualista.core.model;
 
-import java.util.List;
+public interface IEditActor extends IGetActor {
 
-public interface IEditActor {
+	void setName(String newName);
 
-	public String getName();
-
-	public void setName(String newName);
-
-	public Image getImage();
-
-	public void setImage(Image newImage);
-
-	public List<IAction> getActions();
+	void setImage(Image newImage);
 }
