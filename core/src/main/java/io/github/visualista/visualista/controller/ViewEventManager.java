d555d119035581d8469ec9f9dc5abe780d12ab0c
Pierre Krafft <krafftpierre@gmail.com>
2014-05-08 00:35:03 +0200

    Checkstyle cleanup

--- core/src/main/java/io/github/visualista/visualista/controller/ViewEventManager.java
+++ core/src/main/java/io/github/visualista/visualista/controller/ViewEventManager.java
@@ -5,19 +5,24 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ViewEventManager {
-	private final List<ViewEventListener> eventListeners = new ArrayList<ViewEventListener>();
-	
-	public synchronized void addEventListener(ViewEventListener eventListener) {
+	private final List<ViewEventListener> eventListeners =
+			new ArrayList<ViewEventListener>();
+
+	public final synchronized void addEventListener(
+			final ViewEventListener eventListener) {
 		eventListeners.add(eventListener);
 	}
 
-	public synchronized void removeEventListener(ViewEventListener eventListener) {
+	public final synchronized void removeEventListener(
+			final ViewEventListener eventListener) {
 		eventListeners.remove(eventListener);
 	}
-	
-	public synchronized void fireViewEvent() {
-		EditorViewEvent event = new EditorViewEvent(this);
-		Iterator<ViewEventListener> eventIterator = eventListeners.iterator();
+
+	public final synchronized void fireViewEvent() {
+		final EditorViewEvent event = new EditorViewEvent(this,
+				EditorViewEvent.Type.NEW_SCENE);
+		final Iterator<ViewEventListener> eventIterator = eventListeners
+				.iterator();
 		while (eventIterator.hasNext()) {
 			eventIterator.next().handleViewEvent(event);
 		}
