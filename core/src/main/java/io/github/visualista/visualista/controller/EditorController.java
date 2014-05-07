4f68400ff907fae782dedb964204899e68a50a76
Pierre Krafft <krafftpierre@gmail.com>
2014-05-07 21:42:40 +0200

    Made EditorController into a ViewEventListener

--- core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorController.java
@@ -1,10 +1,12 @@
 package io.github.visualista.visualista.controller;
 
+import com.badlogic.gdx.Gdx;
+
 import io.github.visualista.view.VisualistaView;
 import io.github.visualista.visualista.core.Visualista;
 
 
-public class EditorController {
+public class EditorController implements ViewEventListener {
 
 	private Visualista visualista;
 	private VisualistaView view;
@@ -13,6 +15,17 @@ public class EditorController {
 			final VisualistaView view) {
 		this.visualista = visualista;
 		this.view = view;
+		addEventHandlersToView();
+	}
+
+	private void addEventHandlersToView() {
+		view.addViewEventListener(this);
+		
+	}
+
+	@Override
+	public void handleViewEvent(ViewEvent event) {
+		Gdx.app.log("Event", "ViewEvent fired");
 	}
 
 }
