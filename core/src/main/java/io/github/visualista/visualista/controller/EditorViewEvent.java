2d926f2b16a3b5cce6c50446d5931e2030cd5a30
Markus Bergland <markus.bergland@gmail.com>
2014-05-08 15:12:53 +0200

    Modified to match controller

--- core/src/main/java/io/github/visualista/visualista/controller/EditorViewEvent.java
+++ core/src/main/java/io/github/visualista/visualista/controller/EditorViewEvent.java
@@ -5,19 +5,32 @@ import java.util.EventObject;
 public class EditorViewEvent extends EventObject {
     private static final long serialVersionUID = -8033356189055577154L;
 
-    enum Type {
-        NEW_SCENE
+    public enum Type {
+        NEW_SCENE,
+        NEW_ACTOR,
+        SELECT_ACTOR,
+        REMOVE_ACTOR,
+        CHANGE_ACTOR_NAME
     }
 
     private final Type eventType;
+    private final Object sourceObject;
 
-    public EditorViewEvent(final Object source, final Type eventType) {
+    public EditorViewEvent(final Object source, final Type eventType, final Object sourceObject) {
         super(source);
         this.eventType = eventType;
+        this.sourceObject = sourceObject;
+    }
+    
+    public EditorViewEvent(final Object source, final Type eventType){
+        this(source, eventType, null);
     }
 
     public final Type getEventType() {
         return eventType;
     }
-
+    
+    public final Object getSourceObject() {
+        return sourceObject;
+    }
 }
