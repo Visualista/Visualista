a5f4609302f1595858c55ae2fdadde2ceab1540d
Pierre Krafft <krafftpierremail.com>
2014-05-13 18:11:29 +0200

    Rectagle is an actor that draws a single color in its area.

---
+++ core/src/main/java/io/github/visualista/visualista/view/Rectangle.java
@@ -0,0 +1,37 @@
+package io.github.visualista.visualista.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Rectangle extends Actor {
+    private Texture borderTexture;
+
+    @Override
+    public void draw(SpriteBatch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        float x = getX();
+        float y = getY();
+        float width = getWidth();
+        float height = getHeight();
+        batch.draw(borderTexture, x, y, width, height);
+
+    }
+
+    public Rectangle(Color borderColor) {
+        borderTexture = createTexture(borderColor);
+    }
+
+    private static Texture createTexture(Color color) {
+        Pixmap pixmap = new Pixmap(1, 1, Format.RGBA4444);
+        pixmap.setColor(color);
+        pixmap.fill();
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
+        return texture;
+    }
+}
