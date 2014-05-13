31e03beefa4fac2090d1c4f984d98e1dff29a22f
Pierre Krafft <krafftpierremail.com>
2014-05-13 11:07:56 +0200

    Bidirectional map. Use if you want to be able to do reverse-lookups of values to keys.

---
+++ core/src/main/java/io/github/visualista/visualista/util/BiDiMap.java
@@ -0,0 +1,78 @@
+package io.github.visualista.visualista.util;
+
+import java.util.HashMap;
+
+public class BiDiMap<K, V>
+
+{
+
+    private HashMap<K, V> keyToValue;
+
+    private HashMap<V, K> valueToKey;
+
+    public BiDiMap()
+
+    {
+
+        keyToValue = new HashMap<K, V>();
+
+        valueToKey = new HashMap<V, K>();
+
+    }
+
+    public void clear()
+
+    {
+
+        keyToValue.clear();
+
+        valueToKey.clear();
+
+    }
+
+    public int size()
+
+    {
+
+        return keyToValue.size();
+
+    }
+
+    public V put(K key, V val)
+
+    {
+        valueToKey.put(val, key);
+        return keyToValue.put(key, val);
+
+    }
+
+    public V getValue(K key)
+
+    {
+        return keyToValue.get(key);
+
+    }
+
+    public K getKey(V value) {
+        return valueToKey.get(value);
+    }
+
+    public V removeByKey(K key) {
+        V value = keyToValue.get(key);
+        if (value != null) {
+            valueToKey.remove(value);
+            keyToValue.remove(key);
+        }
+        return value;
+    }
+
+    public K removeByValue(V value) {
+        K key = valueToKey.get(value);
+        if (key != null) {
+            keyToValue.remove(key);
+            valueToKey.remove(value);
+        }
+        return key;
+    }
+
+}
