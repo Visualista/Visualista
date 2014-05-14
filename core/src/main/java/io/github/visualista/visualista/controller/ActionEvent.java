09c3b8a881b6cc1871ead72c545b2be9e14483c0
Markus Bergland <markus.bergland@gmail.com>
2014-05-14 15:40:21 +0200

    Refined event handling

--- core/src/main/java/io/github/visualista/visualista/controller/ActionEvent.java
+++ core/src/main/java/io/github/visualista/visualista/controller/ActionEvent.java
@@ -4,22 +4,22 @@ import java.util.EventObject;
 
 public class ActionEvent extends EventObject{
     
-    private final ActionEventType aeType;
-    private final Object newData;
+    public final Object dataObject;
+    public final ActionEventType aeType;
     
-    public ActionEvent(Object source, ActionEventType aeType, Object newData) {
+    public ActionEvent(Object source, ActionEventType newAeType, Object newData) {
         super(source);
-        this.aeType = aeType;
-        this.newData = newData;
+        aeType = newAeType;
+        dataObject = newData;
     }
 
     private static final long serialVersionUID = -5969105349275332629L;
-
-    public ActionEventType getAeType(){
-        return aeType;
+    
+    public Object getDataObject(){
+        return dataObject;
     }
     
-    public Object getNewData(){
-        return newData;
+    public ActionEventType getAeType(){
+        return aeType;
     }
 }
