b12b159043d5872ac966b80abd283de9f905e463
Zalastax <krafftpierre@gmail.com>
2014-05-14 19:30:43 +0200

    Imported TextArea

--- core/src/main/java/io/github/visualista/visualista/view/VisualistaView.java
+++ core/src/main/java/io/github/visualista/visualista/view/VisualistaView.java
@@ -23,6 +23,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
