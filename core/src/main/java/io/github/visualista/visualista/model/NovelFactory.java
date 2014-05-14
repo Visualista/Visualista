a301fde0405229d68780638201faaa081a784647
Zalastax <krafftpierre@gmail.com>
2014-05-14 18:27:54 +0200

    Give each Novel a random name. Used during development

--- core/src/main/java/io/github/visualista/visualista/model/NovelFactory.java
+++ core/src/main/java/io/github/visualista/visualista/model/NovelFactory.java
@@ -6,16 +6,19 @@ public class NovelFactory {
 
     private IdGenerator idGenerator;
     private SceneFactory sceneFactory;
+    private Random random;
 
     public NovelFactory(SceneFactory sceneFactory) {
         this.idGenerator = new IdGenerator();
         this.sceneFactory = sceneFactory;
+        this.random = new Random();
     }
 
     public Novel createNovel() {
         Scene scene = sceneFactory.createScene();
         Novel novel = new Novel(new ArrayList<Scene>());
         novel.addScene(scene);
+        novel.setName(""+random.nextInt(1000));
         return novel;
     }
 }
