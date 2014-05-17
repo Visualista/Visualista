cd4d29fbe99403a4926d8a2ed27c951d9ddd8d94
Pierre Krafft <krafftpierremail.com>
2014-05-17 18:34:13 +0200

    Added a class that helps convert model items to Gdx items

---
+++ core/src/main/java/io/github/visualista/visualista/view/ModelToGdxHelper.java
@@ -0,0 +1,29 @@
+package io.github.visualista.visualista.view;
+
+import java.io.File;
+
+import io.github.visualista.visualista.model.Actor;
+import io.github.visualista.visualista.model.Tile;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public enum ModelToGdxHelper {
+    ;
+
+    public static Image createImageFor(Tile tile) {
+        Actor tileActor = tile.getActor();
+        if (tileActor.hasNoImage()) {
+            return new Image((TextureRegionDrawable)null);
+        }
+        File javaFile = tileActor.getImage().getFile();
+        FileHandle fileFromModel = Gdx.files.absolute(javaFile
+                .getAbsolutePath());
+        return new Image(new TextureRegionDrawable(new TextureRegion(
+                new Texture(fileFromModel))));
+    }
+}
