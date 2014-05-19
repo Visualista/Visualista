bd4934becd698c3137a035a195ab01069502a445
Markus Bergland <markus.bergland@gmail.com>
2014-05-19 11:11:39 +0200

    Added necessary methods

---
+++ core/src/main/java/io/github/visualista/visualista/view/IGetPlayerController.java
@@ -0,0 +1,9 @@
+package io.github.visualista.visualista.view;
+
+import io.github.visualista.visualista.model.IGetActor;
+
+public interface IGetPlayerController {
+    public void addDataToView();
+    
+    public void tileClicked(IGetActor actor);
+}
