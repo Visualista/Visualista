cbafe2fc3c159353338e66dfdc46c3f21d00aac0
Fredokin <fredokin@gmail.com>
2014-05-21 15:24:36 +0200

    Added shell for GridSizeDialog

---
+++ core/src/main/java/io/github/visualista/visualista/view/GridSizeDialog.java
@@ -0,0 +1,25 @@
+package io.github.visualista.visualista.view;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class GridSizeDialog extends Dialog {
+    
+    private final static String title = "Input Grid Size";
+
+    public GridSizeDialog(Skin skin) {
+        super(title, skin);
+        // TODO Auto-generated constructor stub
+    }
+
+    public GridSizeDialog(Skin skin, String windowStyleName) {
+        super(title, skin, windowStyleName);
+        // TODO Auto-generated constructor stub
+    }
+
+    public GridSizeDialog(WindowStyle windowStyle) {
+        super(title, windowStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+}
