16cc81b2b533d70904e5defcb427fac83cade2f6
Zalastax <krafftpierre@gmail.com>
2014-05-22 16:20:42 +0200

    AddActorDialog is now complete.

--- core/src/main/java/io/github/visualista/visualista/view/AddActorDialog.java
+++ core/src/main/java/io/github/visualista/visualista/view/AddActorDialog.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldFilter;
 
 public class AddActorDialog extends Dialog {
 
@@ -29,9 +30,6 @@ public class AddActorDialog extends Dialog {
     private List<IGetActor> list;
     private java.util.List<IGetActor> actorList;
     private IGetActor actor;
-    private Actor placeHolderActor = new Actor();
-    private int placeHolderX = 1;
-    private int placeHolderY = 1;
     private TextField columnInput;
     private TextField rowInput;
     private Dimension gridSize;
@@ -69,12 +67,14 @@ public class AddActorDialog extends Dialog {
         getContentTable().add(
                 new Label("Column (1-" + gridSize.getWidth() + "):", skin));
         columnInput = new TextField("1", skin);
+        columnInput
+                .setTextFieldFilter(createInputFilter(1, gridSize.getWidth()));
         getContentTable().add(columnInput).row();
         getContentTable().add(
                 new Label("Row (1-" + gridSize.getHeight() + "):", skin));
         rowInput = new TextField("1", skin);
+        rowInput.setTextFieldFilter(createInputFilter(1, gridSize.getHeight()));
         getContentTable().add(rowInput);
-
         okButton = new TextButton("OK", skin);
         button(okButton, true);
 
@@ -82,12 +82,30 @@ public class AddActorDialog extends Dialog {
         button(cancelButton, false);
     }
 
+    private TextFieldFilter createInputFilter(final int min, final int max) {
+        return new TextFieldFilter() {
+
+            @Override
+            public boolean acceptChar(TextField textField, char c) {
+                return (Character.isDigit(c)
+                        && Integer.parseInt((textField.getText() + c)) >= min && Integer
+                        .parseInt((textField.getText() + c)) <= max);
+            }
+
+        };
+    }
+
     @Override
     protected void result(Object object) {
         if ((Boolean) object) {
+            String columnText = columnInput.getText();
+            int column = columnText.isEmpty() ? 1 : Integer
+                    .parseInt(columnText);
+            String rowText = rowInput.getText();
+            int row = rowText.isEmpty() ? 1 : Integer.parseInt(rowText);
             eventManager.fireViewEvent(this, Type.ADD_SET_ACTOR_ACTION, actor,
-                    new PositionedActor(new Point(placeHolderX, placeHolderY),
-                            placeHolderActor));
+                    new PositionedActor(new Point(column, row),
+                            (Actor)list.getSelected()));
         }
         super.result(object);
     }
