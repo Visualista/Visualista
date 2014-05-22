e4f773f9083a3b557ad8a45b1b51c20c8aa2899a
Markus Bergland <markus.bergland@gmail.com>
2014-05-22 09:58:45 +0200

    Made nothing

--- core/src/main/java/io/github/visualista/visualista/playercontroller/PlayerController.java
+++ core/src/main/java/io/github/visualista/visualista/playercontroller/PlayerController.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import com.badlogic.gdx.Gdx;
+
 public class PlayerController implements IGetPlayerController{
     
     private VisualistaPlayer visualista;
