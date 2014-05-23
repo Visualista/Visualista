43b070fbc9d98253dad3c7238a0cf029cdef2565
Pierre Krafft <krafftpierremail.com>
2014-05-23 20:50:34 +0200

    Made it possible to select Player or Editor.

--- desktop/src/main/java/io/github/visualista/visualista/java/VisualistaDesktop.java
+++ desktop/src/main/java/io/github/visualista/visualista/java/VisualistaDesktop.java
@@ -1,5 +1,7 @@
 package io.github.visualista.visualista.java;
 
+import java.util.Scanner;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.utils.GdxNativesLoader;
@@ -24,16 +26,22 @@ public class VisualistaDesktop {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = APPLICATION_WIDTH;
         config.height = APPLICATION_HEIGHT;
-        
-        // Note that you will have to change what kind of view you want
-        
-        final PlayerView view = new PlayerView(new Dimension(config.width, config.height));
-        final VisualistaPlayer model = new VisualistaPlayer();
-        new PlayerController(model, view, new DesktopFilePicker());
-        
-//        final EditorView view = new EditorView(new Dimension(config.width,config.height),new DesktopFilePicker());
-//        final VisualistaEditor model = new VisualistaEditor();
-//        new EditorController(model, view);
-        new LwjglApplication(view, config);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write 1 for Player and 2 for Editor. Anything else will exit");
+        String in = scanner.next();
+        if ("1".equals(in)) {
+            final PlayerView view = new PlayerView(new Dimension(config.width,
+                    config.height));
+            final VisualistaPlayer model = new VisualistaPlayer();
+            new PlayerController(model, view, new DesktopFilePicker());
+            new LwjglApplication(view, config);
+        } else if ("2".equals(in)) {
+            final EditorView view = new EditorView(new Dimension(config.width,
+                    config.height), new DesktopFilePicker());
+            final VisualistaEditor model = new VisualistaEditor();
+            new EditorController(model, view);
+            new LwjglApplication(view, config);
+        }
+
     }
 }
