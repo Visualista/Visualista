d2dd18b3a85517081343aad592cb8f543a8819ee
Fredokin <fredokin@gmail.com>
2014-05-23 13:55:01 +0200

    Completed GridTest

--- core/src/test/java/io/github/visualista/visualista/model/GridTest.java
+++ core/src/test/java/io/github/visualista/visualista/model/GridTest.java
@@ -1,9 +1,8 @@
 package io.github.visualista.visualista.model;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import io.github.visualista.visualista.util.Dimension;
-import io.github.visualista.visualista.util.Matrix;
-
 import java.util.Random;
 
 import org.junit.Before;
@@ -14,27 +13,33 @@ public class GridTest {
     private static final int MAX_WIDTH = 150;
     private int width;
     private int height;
+    private Dimension dim;
+    private Grid dimensionGrid;
 
     @Before
     public void setUp() throws Exception {
         Random rand = new Random();
         width = rand.nextInt(MAX_WIDTH) + 1;
         height = rand.nextInt(MAX_HEIGHT) + 1;
+        dim = new Dimension(width,height);
+        dimensionGrid = new Grid(dim);
     }
 
     @Test
     public void testGridDimensionConstructorSameAsIntIntConstructor() {
-        Dimension dim = new Dimension(width, height);
-        Grid dimensionGrid = new Grid(dim);
         Grid intIntGrid = new Grid(width, height);
-        assertEquals(dimensionGrid.getSize(), intIntGrid.getSize());
+        assertThat(dimensionGrid.getSize(), equalTo(intIntGrid.getSize()));
     }
 
     @Test
     public void testGetSize() {
-        Dimension dim = new Dimension(width, height);
-        Grid dimensionGrid = new Grid(dim);
-        assertEquals(dim, dimensionGrid.getSize());
+        assertThat(dim, equalTo(dimensionGrid.getSize()));
+    }
+
+    @Test
+    public void testToString() {
+        assertThat("Grid [Size=" + dim.toString() + "]",
+                equalTo(dimensionGrid.toString()));
     }
 
 }
