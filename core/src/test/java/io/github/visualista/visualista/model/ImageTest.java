bb1cae642e22943b079669a3b6516b3f2bc3958b
Fredokin <fredokin@gmail.com>
2014-05-23 11:17:34 +0200

    Added ImageTest

---
+++ core/src/test/java/io/github/visualista/visualista/model/ImageTest.java
@@ -0,0 +1,21 @@
+package io.github.visualista.visualista.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImageTest {
+
+    private Image image;
+
+    @Before
+    public void setUp(){
+        image = new Image(null);
+    }
+
+    @Test
+    public void testGetFile(){
+        assertNull(image.getFile());
+    }
+}
