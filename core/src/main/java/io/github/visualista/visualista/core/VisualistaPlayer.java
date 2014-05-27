708ac02fb217feca28b5a652bb31154547d90e0d
Zalastax <krafftpierre@gmail.com>
2014-05-27 10:57:04 +0200

    Made PlayerView use the correct openNovel method.

--- core/src/main/java/io/github/visualista/visualista/core/VisualistaPlayer.java
+++ core/src/main/java/io/github/visualista/visualista/core/VisualistaPlayer.java
@@ -2,8 +2,13 @@ package io.github.visualista.visualista.core;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 
 import io.github.visualista.visualista.io.FileLoader;
+import io.github.visualista.visualista.io.Unzipper;
 import io.github.visualista.visualista.model.Actor;
 import io.github.visualista.visualista.model.Novel;
 import io.github.visualista.visualista.util.Point;
@@ -13,6 +18,8 @@ import io.github.visualista.visualista.util.Point;
  * @author Markus Bergland, Erik Risfeltd, Pierre Krafft
  */
 public class VisualistaPlayer {
+    private static final String FILE_FOLDER_PATH = "files" + File.separator;
+    
     private Novel currentNovel;
 
     public VisualistaPlayer() {
@@ -31,17 +38,35 @@ public class VisualistaPlayer {
     }
 
     public Novel openNovel(final File file) {
+        try {
+            Unzipper.unzip(file, fileFolderHandle()
+                    .file());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         final FileLoader<Novel> fileLoader = new FileLoader<Novel>();
         try {
-            setCurrentNovel(fileLoader.getObjectFromFile(file));
-            currentNovel.setCurrentScene(currentNovel.getScenes().get(0));
+            setCurrentNovel(fileLoader.getObjectFromFile(getNovelFileHandle().file()));
         } catch (final FileNotFoundException e) {
             // TODO error message
             e.printStackTrace();
         }
+
         return currentNovel;
 
     }
+    
+    private FileHandle fileFolderHandle() {
+        return Gdx.files.local(FILE_FOLDER_PATH);
+    }
+    
+    private FileHandle getNovelFileHandle() {
+        return Gdx.files.local(
+                FILE_FOLDER_PATH + "novel.xml");
+    }
+    
     public Actor getActorFromPosition(final Point position){
         return currentNovel.getCurrentScene().getGrid().getAt(position)
                 .getActor();
